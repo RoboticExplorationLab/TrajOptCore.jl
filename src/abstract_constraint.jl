6a6b7dfb12b84594b0095f72bd91b20cc3cacddb
Brian Jackson <bjack205@gmail.com>
2020-03-27 16:34:27 -0700

    Change abstract constraint to have zero params

--- src/abstract_constraint.jl
+++ src/abstract_constraint.jl
@@ -12,60 +12,21 @@
 # 	evaluate,
 # 	jacobian
 
+import RobotDynamics: jacobian!
 
-abstract type GeneralConstraint end
 
 "Specifies whether the constraint is an equality or inequality constraint.
 Valid subtypes are `Equality`, `Inequality`, and `Null`"
 abstract type ConstraintSense end
 "Inequality constraints of the form ``h(x) \\leq 0``"
-abstract type Equality <: ConstraintSense end
+struct Equality <: ConstraintSense end
 "Equality constraints of the form ``g(x) = 0``"
-abstract type Inequality <: ConstraintSense end
-abstract type Null <: ConstraintSense end
+struct Inequality <: ConstraintSense end
+
 
 """
-Specifies the ``bandedness'' of the constraint. This ends up being the width
-of the constraint Jacobian, or the total number of input variables. This is
-important to reduce the size of arrays needed to store the Jacobian, as well as
-special-case the block matrix algebra.
+	AbstractConstraint
 
-Current subtypes:
-* [`Stage`](@ref)
-* [`State`](@ref) <: `Stage`
-* [`Control`](@ref) <: `Stage`
-* [`Coupled`](@ref)
-* [`Dynamical`](@ref) <: `Coupled`
-* [`CoupledState`](@ref) <: `Coupled`
-* [`CoupledControl`](@ref) <: `Coupled`
-* [`General`](@ref)
-* [`GeneralState`](@ref) <: `General`
-* [`GeneralControl`](@ref) <: `General`
-"""
-abstract type ConstraintType end
-"Only a function of states and controls at a single knotpoint"
-abstract type Stage <: ConstraintType end
-"Only a function of states at a single knotpoint"
-abstract type State <: Stage end
-"Only a function of controls at a single knotpoint"
-abstract type Control <: Stage end
-"Only a function of states and controls at two adjacent knotpoints"
-abstract type Coupled <: ConstraintType end
-"Only a function of states and two adjacent knotpoints,
-and the control at the previous knotpoint, i.e. f(x,u) - x′"
-abstract type Dynamical <: Coupled end
-"Only a function of states at adjacent knotpoints"
-abstract type CoupledState <: Coupled end
-"Only a function of controls at adjacent knotpoints"
-abstract type CoupledControl <: Coupled end
-"A function of all states and controls in the trajectory"
-abstract type General <: ConstraintType end
-"A function of all states in the trajectory"
-abstract type GeneralState <: General end
-"A function of all controls in the trajectory"
-abstract type GeneralControl <: General end
-
-""" $(TYPEDEF)
 Abstract vector-valued constraint of size `P` for a trajectory optimization problem.
 May be either inequality or equality (specified by `S<:ConstraintSense`), and be function of
 single, adjacent, or all knotpoints (specified by `W<:ConstraintType`).
@@ -106,65 +67,76 @@ jacobian!(∇c::Vector{<:AbstractMatrix}, ::MyCon, Z::Traj, inds=1:length(Z)-1)
 These methods can be specified for any constraint, instead of the not-in-place functions
 	above.
 """
-abstract type AbstractConstraint{S<:ConstraintSense,W<:ConstraintType,P} <: GeneralConstraint end
+abstract type AbstractConstraint end
 
-# Getters
-"Get type of constraint (bandedness)"
-contype(::AbstractConstraint{S,W}) where {S,W} = W
+"Only a function of states and controls at a single knotpoint"
+abstract type StageConstraint <: AbstractConstraint end
+"Only a function of states at a single knotpoint"
+abstract type StateConstraint <: StageConstraint end
+"Only a function of controls at a single knotpoint"
+abstract type ControlConstraint <: StageConstraint end
+"Only a function of states and controls at two adjacent knotpoints"
+abstract type CoupledConstraint <: AbstractConstraint end
+"Only a function of states at adjacent knotpoints"
+abstract type CoupledStateConstraint <: CoupledConstraint end
+"Only a function of controls at adjacent knotpoints"
+abstract type CoupledControlConstraint <: CoupledConstraint end
+
+const StateConstraints = Union{StageConstraint, StateConstraint, CoupledConstraint, CoupledStateConstraint}
+const ControlConstraints = Union{StageConstraint, ControlConstraint, CoupledConstraint, CoupledControlConstraint}
+
 "Get constraint sense (Inequality vs Equality)"
-sense(::AbstractConstraint{S}) where S = S
+sense(::C) where C <: AbstractConstraint = throw(NotImplemented(:sense, Symbol(C)))
+"Get type of constraint (bandedness)"
+contype(::C) where C <: AbstractConstraint = throw(NotImplemented(:contype, Symbol(C)))
+"Dimension of the state vector"
+RobotDynamics.state_dim(::C) where C <: StateConstraint = throw(NotImplemented(:state_dim, Symbol(C)))
+"Dimension of the control vector"
+RobotDynamics.control_dim(::C) where C <: ControlConstraint = throw(NotImplemented(:control_dim, Symbol(C)))
+"Return the constraint value"
+evaluate(::C) where C <: AbstractConstraint = throw(NotImplemented(:evaluate, Symbol(C)))
+"Length of constraint vector"
+Base.length(::C) where C <: AbstractConstraint = throw(NotImplemented(:length, Symbol(C)))
+
+Base.size(con::AbstractConstraint) = (length(con), width(con))
 
 "Returns the width of the constraint Jacobian, i.e. the total number of inputs
 to the constraint"
 width(con::AbstractConstraint) = sum(widths(con))
 
-width(::Type{Stage},N,M) = N+M
-width(::Type{State},N,M) = N
-width(::Type{Control},N,M) = M
-width(::Type{Coupled},N,M) = 2N + 2M
-width(::Type{Dynamical},N,M) = 2N + M
-width(::Type{CoupledState},N,M) = 2N
-width(::Type{CoupledControl},N,M) = 2M
+width(::StageConstraint,n,m) = n+m
+width(::StateConstraint,n,m) = n
+width(::ControlConstraint,n,m) = m
+width(::CoupledConstraint,n,m) = 2n + 2m
+width(::CoupledStateConstraint,n,m) = 2n
+width(::CoupledControlConstraint,n,m) = 2m
 
-widths(con::AbstractConstraint{<:Any,Stage}) = (state_dim(con) + control_dim(con),)
-widths(con::AbstractConstraint{<:Any,State}) = (state_dim(con),)
-widths(con::AbstractConstraint{<:Any,Control}) = (control_dim(con),)
-widths(con::AbstractConstraint{<:Any,Coupled}) = (state_dim(con) + control_dim(con), state_dim(con) + control_dim(con))
-widths(con::AbstractConstraint{<:Any,Dynamical}) = (state_dim(con) + control_dim(con), state_dim(con))
-widths(con::AbstractConstraint{<:Any,CoupledState}) = (state_dim(con), state_dim(con))
-widths(con::AbstractConstraint{<:Any,CoupledControl}) = (control_dim(con), control_dim(con))
-widths(con::AbstractConstraint{<:Any,<:General}) = (Inf,)
+widths(con::StageConstraint) = (state_dim(con) + control_dim(con),)
+widths(con::StateConstraint) = (state_dim(con),)
+widths(con::ControlConstraint) = (control_dim(con),)
+widths(con::CoupledConstraint) = (state_dim(con) + control_dim(con), state_dim(con) + control_dim(con))
+widths(con::CoupledStateConstraint) = (state_dim(con), state_dim(con))
+widths(con::CoupledControlConstraint) = (control_dim(con), control_dim(con))
 
 "Upper bound of the constraint, as a vector, which is 0 for all constraints
 (except bound constraints)"
-upper_bound(con::AbstractConstraint{Inequality,W,P}) where {P,W} = @SVector zeros(P)
-upper_bound(con::AbstractConstraint{Equality,W,P}) where {P,W} = @SVector zeros(P)
+@inline upper_bound(con::AbstractConstraint) = upper_bound(sense(con)) * @SVector ones(length(con))
+@inline upper_bound(::Inequality) = 0.0
+@inline upper_bound(::Equality) = 0.0
 
 "Upper bound of the constraint, as a vector, which is 0 equality and -Inf for inequality
 (except bound constraints)"
-lower_bound(con::AbstractConstraint{Inequality,W,P}) where {P,W} = -Inf*@SVector ones(P)
-lower_bound(con::AbstractConstraint{Equality,W,P}) where {P,W} = @SVector zeros(P)
+@inline lower_bound(con::AbstractConstraint) = lower_bound(sense(con)) * @SVector ones(length(con))
+@inline lower_bound(::Inequality) = -Inf
+@inline lower_bound(::Equality) = 0.0
 
 "Is the constraint a bound constraint or not"
 @inline is_bound(con::AbstractConstraint) = false
 
 "Check whether the constraint is consistent with the specified state and control dimensions"
-@inline check_dims(con::AbstractConstraint{S,State},n,m) where S = state_dim(con) == n
-@inline check_dims(con::AbstractConstraint{S,Control},n,m) where S = control_dim(con) == m
-@inline function check_dims(con::AbstractConstraint{S,W},n,m) where {S,W<:ConstraintType}
-	state_dim(con) == n && control_dim(con) == m
-end
-
-"Size of state vector"
-control_dims(::AbstractConstraint{S,State}) where S =
-	throw(ErrorException("Cannot get control dimension from a state-only constraint"))
-
-"Size of control vector"
-state_dims(::AbstractConstraint{S,Control}) where S =
-	throw(ErrorException("Cannot get state dimension from a control-only constraint"))
-
-Base.length(::AbstractConstraint{S,W,P}) where {S,W,P} = P
-Base.size(con::AbstractConstraint) = length(con), width(con)
+@inline check_dims(con::StateConstraint,n,m) = state_dim(con) == n
+@inline check_dims(con::ControlConstraint,n,m) = control_dim(con) == m
+@inline check_dims(con::AbstractConstraint,n,m) = state_dim(con) == n && control_dim(con) == m
 
 con_label(::AbstractConstraint, i::Int) = "index $i"
 
@@ -185,15 +157,15 @@ For W<:Coupled this will loop over calls to `evaluate(con,Z[k+1],Z[k])`
 For W<:General,this must function must be explicitly defined. Other types may define it
 	if desired.
 """
-function evaluate!(vals::Vector{<:AbstractVector}, con::AbstractConstraint{P,<:Stage},
-		Z::Traj, inds=1:length(Z)) where P
+function evaluate!(vals::Vector{<:AbstractVector}, con::StageConstraint,
+		Z::Traj, inds=1:length(Z))
 	for (i,k) in enumerate(inds)
 		vals[i] = evaluate(con, Z[k])
 	end
 end
 
-function evaluate!(vals::Vector{<:AbstractVector}, con::AbstractConstraint{P,<:Coupled},
-		Z::Traj, inds=1:length(Z)-1) where P
+function evaluate!(vals::Vector{<:AbstractVector}, con::CoupledConstraint,
+		Z::Traj, inds=1:length(Z)-1)
 	for (i,k) in enumerate(inds)
 		vals[i] = evaluate(con, Z[k+1], Z[k])
 	end
@@ -213,41 +185,83 @@ For W<:Coupled this will loop over calls to `jacobian(con,Z[k+1],Z[k])`
 For W<:General,this must function must be explicitly defined. Other types may define it
 	if desired.
 """
-function jacobian!(∇c::VecOrMat{<:AbstractMatrix}, con::AbstractConstraint{<:Any,<:Stage},
+function jacobian!(∇c::VecOrMat{<:AbstractMatrix}, con::StageConstraint,
 		Z::Traj, inds=1:length(Z))
 	for (i,k) in enumerate(inds)
-		jacobian!(∇c[i], con, Z[k])
+		jacobian!(∇c[i], con, Z[k], 1)
 	end
 end
 
-# Default methods for converting KnotPoints to states and controls
-for method in [:evaluate]#, :jacobian]
-	@eval begin
-			@inline $(method)(con::AbstractConstraint{S,Stage},   Z::AbstractKnotPoint) where S = $(method)(con, state(Z), control(Z))
-			@inline $(method)(con::AbstractConstraint{S,State},   Z::AbstractKnotPoint) where S = $(method)(con, state(Z))
-			@inline $(method)(con::AbstractConstraint{S,Control}, Z::AbstractKnotPoint) where S = $(method)(con, control(Z))
-
-			@inline $(method)(con::AbstractConstraint{S,Coupled}, Z′::AbstractKnotPoint, Z::AbstractKnotPoint) where S =
-				$(method)(con, state(Z′), control(Z′), state(Z), control(Z))
-			@inline $(method)(con::AbstractConstraint{S,Dynamical}, Z′::AbstractKnotPoint, Z::AbstractKnotPoint) where S =
-				$(method)(con, state(Z′), state(Z), control(Z))
-			@inline $(method)(con::AbstractConstraint{S,CoupledState}, Z′::AbstractKnotPoint, Z::AbstractKnotPoint) where S =
-				$(method)(con, state(Z′), state(Z))
-			@inline $(method)(con::AbstractConstraint{S,CoupledControl}, Z′::AbstractKnotPoint, Z::AbstractKnotPoint) where S =
-				$(method)(con, control(Z′), control(Z))
+function jacobian!(∇c::VecOrMat{<:AbstractMatrix}, con::CoupledConstraint,
+		Z::Traj, inds=1:length(Z))
+	for (i,k) in enumerate(inds)
+		jacobian!(∇c[i], con, Z[k+1], Z[k], 1)
+		jacobian!(∇c[i], con, Z[k+1], Z[k], 2)
 	end
 end
 
-# Default method for converting KnotPoints to states and controls
-@inline jacobian!(∇c, con::AbstractConstraint{<:Any,State}, z::AbstractKnotPoint, i=1) =
+# Default methods for converting KnotPoints to states and controls for StageConstraints
+@inline evaluate(con::StateConstraint, z::AbstractKnotPoint) = evaluate(con, state(z))
+@inline evaluate(con::ControlConstraint, z::AbstractKnotPoint) = evaluate(con, control(z))
+@inline evaluate(con::StageConstraint, z::AbstractKnotPoint) = evaluate(con, state(z), control(z))
+
+@inline jacobian!(∇c, con::StateConstraint, z::AbstractKnotPoint, i=1) =
 	jacobian!(∇c, con, state(z))
-@inline jacobian!(∇c, con::AbstractConstraint{<:Any,Control}, z::AbstractKnotPoint, i=1) =
+@inline jacobian!(∇c, con::ControlConstraint, z::AbstractKnotPoint, i=1) =
 	jacobian!(∇c, con, control(z))
-@inline jacobian!(∇c, con::AbstractConstraint{<:Any,<:Coupled}, z::AbstractKnotPoint, i=1) =
-	jacobian!(∇c, con, z, i)
+@inline jacobian!(∇c, con::StageConstraint, z::AbstractKnotPoint, i=1) =
+	jacobian!(∇c, con, state(z), control(z))
 
-function jacobian!(∇c, con::AbstractConstraint{<:Any,W}, x::SVector) where {W<:Union{State,Control}}
+# ForwardDiff jacobians that are of only state or control
+function jacobian!(∇c, con::Union{StageConstraint,ControlConstraint}, x::StaticVector)
 	eval_c(x) = evaluate(con, x)
 	∇c .= ForwardDiff.jacobian(eval_c, x)
 	return false
 end
+
+function jacobian!(∇c, con::StageConstraint, z::AbstractKnotPoint)
+	eval_c(x) = evaluate(con, StaticKnotPoint(z, x))
+	∇c .= ForwardDiff.jacobian(eval_c, z.z)
+	return false
+end
+
+@inline gen_jacobian(con::AbstractConstraint) = SizedMatrix{size(con)...}(zeros(size(con)))
+
+############################################################################################
+#					             CONSTRAINT LIST										   #
+############################################################################################
+struct ConstraintList
+	n::Int
+	m::Int
+	constraints::Vector{AbstractConstraint}
+	inds::Vector{UnitRange{Int}}
+	p::Vector{Int}
+	function ConstraintList(n::Int, m::Int, N::Int)
+		constraints = AbstractConstraint[]
+		inds = UnitRange{Int}[]
+		p = zeros(Int,N)
+		new(n, m, constraints, inds, p)
+	end
+end
+
+function add_constraint!(cons::ConstraintList, con::AbstractConstraint, inds::UnitRange{Int}, idx=-1)
+	@assert check_dims(con, cons.n, cons.m) "New constaint not consistent with n=$(cons.n) and m=$(cons.m)"
+	@assert inds[end] <= length(cons.p) "Invalid inds, inds[end] must be less than number of knotpoints, $(length(cons.p))"
+	if idx == -1
+		push!(cons.constraints, con)
+		push!(cons.inds, inds)
+	elseif 0 < idx <= length(cons)
+		insert!(cons.constraints, idx, con)
+		insert!(cons.inds, idx, inds)
+	else
+		throw(ArgumentError("cannot insert constraint at index=$idx. Length = $(length(cons))"))
+	end
+	@assert length(cons.constraints) == length(cons.inds)
+end
+
+@inline add_constraint!(cons::ConstraintList, con::AbstractConstraint, k::Int, idx=-1) =
+	add_constraint!(cons, con, k:k, idx)
+
+@inline Base.length(cons::ConstraintList) = length(cons.constraints)
+
+@inline Base.getindex(cons::ConstraintList, i::Int) = cons.constraints[i]
---
+++ test/constraints.jl
@@ -0,0 +1,147 @@
+using StaticArrays
+using RobotDynamics
+using BenchmarkTools
+using LinearAlgebra
+using ForwardDiff
+using UnsafeArrays
+
+n,m = 13,4
+x = @SVector rand(n)
+u = @SVector rand(m)
+z = KnotPoint(x,u,0.1)
+
+# Goal Constraint
+xf = @SVector rand(n)
+con = GoalConstraint(xf)
+size(con) == (n,n)
+evaluate(con, z) == x - xf
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+∇c == I(n)
+
+inds = 1:3
+con = GoalConstraint(xf, inds)
+size(con) == (3,n)
+evaluate(con, z) == (x-xf)[1:3]
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+∇c == Matrix(I,3,n)
+
+# Linear Constraint
+p = 7
+A = rand(p,n+m)
+b = rand(p)
+con = LinearConstraint(n,m,A,b,Inequality())
+size(con) == (p,n+m)
+evaluate(con, z) == A*[x;u] - b
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+∇c == A
+con.A isa StaticMatrix
+con.b isa StaticVector
+
+# Circle Constraint
+p = 4
+xc = @SVector rand(p)
+yc = @SVector rand(p)
+rad = @SVector rand(p)
+con = CircleConstraint(n, xc, yc, rad)
+size(con) == (p,n)
+evaluate(con, z) ≈ @. rad^2 - (xc - x[1])^2 - (yc - x[2])^2
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+sum(∇c[:,3:end]) ≈ 0
+
+∇c ≈ ForwardDiff.jacobian(x->evaluate(con,x), x)
+@btime ForwardDiff.jacobian(x->evaluate($con,x), $x)
+
+
+# Sphere Constraint
+p = 5
+xc = @SVector rand(p)
+yc = @SVector rand(p)
+zc = @SVector rand(p)
+rad = @SVector rand(p)
+con = SphereConstraint(n, xc, yc, zc, rad)
+size(con) == (p,n)
+evaluate(con, z) ≈ @. rad^2 - (xc - x[1])^2 - (yc - x[2])^2 - (zc - x[3])^2
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+sum(∇c[:,4:end]) ≈ 0
+∇c ≈ ForwardDiff.jacobian(x->evaluate(con,x), x)
+
+# Collision Constraint
+x1 = @SVector [1,2,3]
+x2 = x1 .+ 7
+rad = rand()
+con = CollisionConstraint(n, x1, x2, rad)
+size(con) == (1,n)
+evaluate(con, z)[1] ≈ rad^2 - norm(x[x1] - x[x2])^2
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+∇c ≈ ForwardDiff.jacobian(x->evaluate(con,x), x)
+
+# State Norm
+con = NormConstraint(n, m, 1.0, Inequality(), :state)
+size(con) == (1,n+m)
+evaluate(con, z)[1] ≈ x'x - 1
+∇c = gen_jacobian(con)
+jacobian!(∇c, con, z)
+∇c[:] ≈ [2*x; zeros(m)]
+∇c ≈ ForwardDiff.jacobian(x->evaluate(con,StaticKnotPoint(z,x)), z.z)
+
+# Bounds Constraint
+xmin = fill(-Inf,n)
+xmax = fill(Inf,n)
+xmin[1:3] .= -10
+xmax[1:3] .= 10
+umin = -4
+umax = 4
+bnd = BoundConstraint(n,m, x_min=xmin, x_max=xmax, u_min=umin, u_max=umax)
+size(bnd) == (14,n+m)
+evaluate(bnd, z) ≈ filter(isfinite,[x .- xmax; u .- umax; xmin .- x; umin .- u])
+∇c = gen_jacobian(bnd)
+jacobian!(∇c, bnd, z)
+B = Diagonal(@SVector ones(n+m))
+active = isfinite.([xmax;fill(umax,m);xmin;fill(umin,m)])
+∇c ≈ [B;-B][active,:]
+
+# Indexed Constraint
+n_, m_ = 14, n+m
+x_ = push(x, rand())
+u_ = [u; x ./ 10]
+z_ = KnotPoint(x_, u_, z.dt)
+
+icon = IndexedConstraint(n_, m_, bnd)
+size(icon) == (length(bnd), n_ + m_)
+evaluate(icon, z_) == evaluate(bnd, z)
+∇c = gen_jacobian(icon)
+∇c0 = gen_jacobian(bnd)
+jacobian!(∇c, icon, z_) == jacobian!(∇c0, bnd, z)
+∇c[:,1:n] ≈ ∇c0[:,1:n]
+∇c0[:,n .+ (1:m)] ≈ ∇c[:,n_ .+ (1:m)]
+
+icon = IndexedConstraint(n_, m_, con)
+size(icon) == (length(con), n_+m_)
+evaluate(icon, z_) == evaluate(con, z)
+∇c = gen_jacobian(icon)
+∇c0 = gen_jacobian(con)
+jacobian!(∇c, icon, z_) == jacobian!(∇c0, con, z)
+∇c0[1:n] ≈ ∇c[1:n]
+
+
+# Constraint List
+N = 11
+cons = ConstraintList(n,m,N)
+add_constraint!(cons, bnd, 1:N-1)
+cons.constraints[1] === bnd
+cons.inds[1] == 1:N-1
+add_constraint!(cons, con, N)
+cons.constraints[2] === con
+cons.inds[2] == N:N
+
+goal = GoalConstraint(xf)
+add_constraint!(cons, goal, N, 1)
+cons[1] === goal
+cons.inds[1] == N:N
+length(cons) == 3
